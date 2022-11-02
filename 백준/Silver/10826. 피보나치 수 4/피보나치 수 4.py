a35f2cd9dcb42f61278743781a5daf44c3b14391
Junhee Lee <96717686+Ijhee@users.noreply.github.com>
2022-11-02 21:26:10 +0900

    [Silver V] Title: 피보나치 수 4, Time: 72 ms, Memory: 30840 KB -BaekjoonHub

--- 백준/Silver/10826. 피보나치 수 4/피보나치 수 4.py
+++ 백준/Silver/10826. 피보나치 수 4/피보나치 수 4.py
@@ -1,11 +1,6 @@
-f1 = 0; f2 = 1
-lst = []
+a = 0 ; b = 1 ; c = 1
 N = int(input())
-for i in range(N+1):
-    if i == 0:
-        lst.append(f1)
-    elif i == 1:
-        lst.append(f2)
-    else:
-        lst.append(lst[i-1] + lst[i-2])
-print(lst[-1])
+for i in range(N):
+    c = a
+    a = a+b ; b = c
+print(a)
